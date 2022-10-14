1801ad03a2ff1440cbde13fe8650fc3169a95e58
Eric Oliveira <eriicsilva28@gmail.com>
2022-10-13 22:16:31 -0300

    Usuario List/Produtos

--- src/store/index.js
+++ src/store/index.js
@@ -18,7 +18,8 @@ export default new Vuex.Store({
       bairro: '',
       cidade: '',
       estado: ''
-    }
+    },
+    usuario_produtos: []
   },
   getters: {
   },
@@ -28,9 +29,22 @@ export default new Vuex.Store({
     },
     UPDATE_USUARIO(state, payload){
       state.usuario = Object.assign(state.usuario, payload)
+    },
+    UPDATE_USUARIO_PRODUTOS(state,payload) {
+      state.usuario_produtos = payload
+    },
+    ADD_USUARIO_PRODUTOS(state,payload) {
+      state.usuario_produtos.unshift(payload)
     }
   },
   actions: {
+    getUsuarioProdutos(context) {
+      api.get(`/produto?usuario_id=${context.state.usuario.id}`)
+        .then(r => {
+          context.commit('UPDATE_USUARIO_PRODUTOS', r.data)
+        })
+    // incrementando lista dos meus produtos
+    },
     getUsuario(context, payload){
       api.get(`/usuario/${payload}`)
         .then(r => {
--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,13 +1,52 @@
 <template>
-  <p>Lista de Produtos</p>
+  <section>
+    <h2>Adicionar Produto</h2>
+    <ProdutosAdicionar />
+    <h2>Seus Produtos</h2>
+
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, i) in usuario_produtos" :key="i">
+        <ProdutosItem  :produto='produto'>
+         <p>{{produto.descricao}}</p>
+
+        </ProdutosItem>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
+import ProdutosAdicionar from '@/components/ProdutosAdicionar.vue'
+import ProdutosItem from '@/components/ProdutosItem.vue'
+import { mapState, mapActions } from 'vuex'
+
 export default {
+  name: 'UsuarioProdutos',
+  components: { ProdutosAdicionar, ProdutosItem},
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos()
+    }
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos()
+    }
+  }
 
 }
 </script>
 
-<style>
+<style scoped>
+
+h2 {
+  margin-bottom: 20px;
+}
 
 </style>
